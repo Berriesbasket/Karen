df1f4283963f04c23078e145dce62806249dbbca
karenchan <berriesbasket@gmail.com>
2024-07-15 22:43:43 -0700

    changed settings

--- multiply.py
+++ multiply.py
@@ -10,8 +10,8 @@ def multiply(num_1, num_2):
     if num_2 == 1:
         return num_1
 
-    return num_1 + multiply(num_1 , num_2 - 1)
+    return num_1 + multiply(num_1, num_2 - 1)
 
 
 
-print(multiply(5, 7))
+print(multiply(10, 7))
